fbf958710ba0b8b78c7565199f8dc03554de6546
simon-lentz <simonlentz1@gmail.com>
2024-04-19 14:56:41 -0400

    refactor matching loops, add parsing for county issuers

--- neo4j/scripts/financial/parse_county_issuer_location.py
+++ neo4j/scripts/financial/parse_county_issuer_location.py
@@ -2,6 +2,7 @@ import os
 import re
 import logging
 import pandas as pd
+from typing import List
 from pathlib import Path
 
 # Set chained assigned opt for pd
@@ -73,94 +74,70 @@ def load_issuer_data(filepath: Path) -> pd.DataFrame:
         raise
 
 
-def prepare_city_place_data(state_abbr: str) -> tuple:
+def prepare_county_place_data(state_abbr: str) -> tuple:
     # Load place data
     place_data = PLACE_DATA_DIR / state_abbr / "place_data.csv"
     place_df = load_place_data(place_data)
     original_place_df = place_df.copy()
-    place_df = place_df.dropna(subset=["CBSA Code"])
-    place_df = place_df[['CBSA Code', 'CBSA Title', 'Metropolitan Division Title', 'CSA Title']].fillna('')
+    place_df = place_df[['County Name', 'County FIPS']]
+    matching_set = []
+    for _, entry in place_df.iterrows():
+        name = entry['County Name']
+        fips = entry['County FIPS']
+        match_tuple = (name, fips)
+        matching_set.append(match_tuple)
 
-    # Clean titles
-    def clean_title(title):
-        first_part = title.split(',')[0].strip().lower()
-        cleaned_title = first_part.replace('-', ',')
-        return cleaned_title
-
-    place_df['CBSA Title'] = place_df['CBSA Title'].apply(clean_title)
-    place_df['Metropolitan Division Title'] = place_df['Metropolitan Division Title'].apply(clean_title)
-    place_df['CSA Title'] = place_df['CSA Title'].apply(clean_title)
-
-    # Create the matching map
-    matching_map = {}
-    for index, row in place_df.iterrows():
-        places = set()
-        places.update(row['CBSA Title'].split(',') + row['Metropolitan Division Title'].split(',') + row['CSA Title'].split(','))  # noqa:E501
-        places.discard('')
-        if places:
-            matching_map[row['CBSA Code']] = list(places)
-
-    return original_place_df, matching_map
+    return original_place_df, matching_set
 
 
-def prepare_city_issuer_data(state_abbr: str) -> tuple:
-    # Load merged city issuers file
-    city_issuers_file = FINANCIAL_DATA_DIR / state_abbr / "merged_city_issuers.csv"
-    city_issuers_df = load_issuer_data(city_issuers_file)
-    city_issuers_original_df = city_issuers_df.copy()
+def prepare_county_issuer_data(state_abbr: str) -> tuple:
+    # Load merged county issuers file
+    county_issuers_file = FINANCIAL_DATA_DIR / state_abbr / "merged_county_issuers.csv"
+    county_issuers_df = load_issuer_data(county_issuers_file)
+    county_issuers_original_df = county_issuers_df.copy()
 
     # Filter and preprocess
-    city_issuers_df = city_issuers_df[['MSRB Issuer Identifier', 'Issuer Name', 'Issue Description']].fillna('')
+    county_issuers_df = county_issuers_df[['MSRB Issuer Identifier', 'Issuer Name', 'Issue Description']].fillna('')
 
     # Function to replace non-alphanumeric characters with spaces
     def clean_text(text):
         # Replace non-alphanumeric characters with spaces
         return re.sub(r'[^a-zA-Z0-9\s]', ' ', text).strip().lower()
 
-    city_issuers_df['Issuer Name'] = city_issuers_df['Issuer Name'].apply(clean_text)
-    city_issuers_df['Issue Description'] = city_issuers_df['Issue Description'].apply(clean_text)
+    county_issuers_df['Issuer Name'] = county_issuers_df['Issuer Name'].apply(clean_text)
+    county_issuers_df['Issue Description'] = county_issuers_df['Issue Description'].apply(clean_text)
 
-    return city_issuers_original_df, city_issuers_df
+    return county_issuers_original_df, county_issuers_df
 
 
-def match_issuers(matching_map: dict, city_issuers_df: pd.DataFrame) -> pd.DataFrame:
-    # Initialize a list to collect data for the DataFrame
-    matches = []
+def match_issuers(matching_set: List[tuple], county_issuers_df: pd.DataFrame) -> pd.DataFrame:
+    # Initialize an empty DataFrame for matched issuers
+    matched_columns = ['MSRB Issuer Identifier', 'County FIPS']
+    matched_df = pd.DataFrame(columns=matched_columns)
 
     # Iterate over each issuer and match based on the place names in the matching map
-    for index, issuer in city_issuers_df.iterrows():
+    for _, issuer in county_issuers_df.iterrows():
         issuer_name = issuer['Issuer Name']
         issue_description = issuer['Issue Description']
-        for cbsa_code, places in matching_map.items():
-            # Check if any place name from the matching map is a substring of the issuer name
-            matched = False
-            for place in places:
-                if place in issuer_name:
-                    matches.append({
-                        'MSRB Issuer Identifier': issuer['MSRB Issuer Identifier'],
-                        'CBSA Code': cbsa_code
-                    })
-                    matched = True
-                    break  # Stop checking after the first match for this issuer
-            if not matched:  # Only check issue description if no match was found in issuer name
-                for place in places:
-                    if place in issue_description:
-                        matches.append({
-                            'MSRB Issuer Identifier': issuer['MSRB Issuer Identifier'],
-                            'CBSA Code': cbsa_code
-                        })
-                        break  # Stop checking after the first match for this issuer
+        for county_name, county_fips in matching_set:
+            # Check if county name is a substring of the issuer name or issue description
+            if county_name in issuer_name or county_name in issue_description:
+                match = pd.DataFrame({
+                    'MSRB Issuer Identifier': [issuer['MSRB Issuer Identifier']],
+                    'County FIPS': [county_fips]
+                })
+                matched_df = pd.concat([matched_df, match], ignore_index=True)
+                # If only the first match is needed, uncomment the next line:
+                # break
 
-    # Create DataFrame from list of matches
-    matched_df = pd.DataFrame(matches)
     return matched_df
 
 
 def merge_dfs(matched: pd.DataFrame, place: pd.DataFrame, issuers: pd.DataFrame) -> pd.DataFrame:
     # Merge the matched df on "CBSA Code" with the original place df
-    merged_with_place = pd.merge(matched, place, on="CBSA Code", how="left")
+    merged_with_place = pd.merge(matched, place, on="County FIPS", how="left")
 
-    # Then merge again on "MSRB Issuer Identifier" with original city issuer df
+    # Then merge again on "MSRB Issuer Identifier" with original county issuer df
     final_merged_df = pd.merge(merged_with_place, issuers, on="MSRB Issuer Identifier", how="left")
 
     # Rename 'State FIPS_x' to 'State FIPS' if it exists
@@ -170,7 +147,6 @@ def merge_dfs(matched: pd.DataFrame, place: pd.DataFrame, issuers: pd.DataFrame)
     # Reorder and select specific columns for the final output
     desired_columns = [
         'Issuer Name', 'Issuer Type', 'Issuer Homepage', 'MSRB Issuer Identifier',
-        'CBSA Code', 'CBSA Title', 'Metropolitan Division Title', 'CSA Title',
         'County Name', 'County FIPS', 'State Abbreviation', 'State FIPS', 'Date Retrieved'
     ]
 
@@ -183,48 +159,48 @@ def merge_dfs(matched: pd.DataFrame, place: pd.DataFrame, issuers: pd.DataFrame)
     return final_merged_df
 
 
-def parse_city_issuers(state_abbr: str) -> None:
+def parse_county_issuers(state_abbr: str) -> None:
     # Load and filter input data
-    original_city_issuer_df, city_issuers_df = prepare_city_issuer_data(state_abbr)
-    original_place_df, matching_map = prepare_city_place_data(state_abbr)
+    original_county_issuer_df, county_issuers_df = prepare_county_issuer_data(state_abbr)
+    original_place_df, matching_map = prepare_county_place_data(state_abbr)
 
-    # Use matching map and filtered issuers df to create a matched city issuers df
-    matched_city_issuers_df = match_issuers(matching_map, city_issuers_df)
+    # Use matching map and filtered issuers df to create a matched county issuers df
+    matched_county_issuers_df = match_issuers(matching_map, county_issuers_df)
 
     # Merge matched and original data
-    merged_df = merge_dfs(matched_city_issuers_df, original_place_df, original_city_issuer_df)
+    merged_df = merge_dfs(matched_county_issuers_df, original_place_df, original_county_issuer_df)
 
     # Output final merged DataFrame to CSV
-    output_file = FINANCIAL_DATA_DIR / state_abbr / "city_issuers.csv"
+    output_file = FINANCIAL_DATA_DIR / state_abbr / "county_issuers.csv"
     merged_df.to_csv(output_file, index=False)
-    logging.info(f"Matched city issuers data written to {output_file}")
+    logging.info(f"Matched county issuers data written to {output_file}")
 
     # Find difference of set of matched issuer ids and original input issuer ids
-    matched_ids = set(matched_city_issuers_df['MSRB Issuer Identifier'])
-    original_ids = set(original_city_issuer_df['MSRB Issuer Identifier'])
+    matched_ids = set(matched_county_issuers_df['MSRB Issuer Identifier'])
+    original_ids = set(original_county_issuer_df['MSRB Issuer Identifier'])
     unmatched_ids = original_ids.difference(matched_ids)
 
-    unmatched_issuers_df = original_city_issuer_df[original_city_issuer_df['MSRB Issuer Identifier'].isin(unmatched_ids)]  # noqa:E501
+    unmatched_issuers_df = original_county_issuer_df[original_county_issuer_df['MSRB Issuer Identifier'].isin(unmatched_ids)]  # noqa:E501
 
-    unmatched_output_file = FINANCIAL_DATA_DIR / state_abbr / "unmatched_city_issuers.csv"
+    unmatched_output_file = FINANCIAL_DATA_DIR / state_abbr / "unmatched_county_issuers.csv"
     unmatched_issuers_df.to_csv(unmatched_output_file, index=False)
-    logging.info(f"Unmatched city issuers data written to {unmatched_output_file}")
+    logging.info(f"Unmatched county issuers data written to {unmatched_output_file}")
 
 
-def parse_city_issuer_location(state_abbr: str):
+def parse_county_issuer_location(state_abbr: str):
     # construct file paths
-    city_merged_issuers_file = FINANCIAL_DATA_DIR / state_abbr / "merged_city_issuers.csv"
+    county_merged_issuers_file = FINANCIAL_DATA_DIR / state_abbr / "merged_county_issuers.csv"
 
-    # if city issuers exist for state, parse them
-    if os.path.exists(city_merged_issuers_file):
-        parse_city_issuers(state_abbr)
+    # if county issuers exist for state, parse them
+    if os.path.exists(county_merged_issuers_file):
+        parse_county_issuers(state_abbr)
     else:
-        logging.info(f"No City issuers found for {state_abbr}")
+        logging.info(f"No county issuers found for {state_abbr}")
 
 
 def parse_by_state():
     for state_abbr, _ in STATE_LOCATORS:
-        parse_city_issuer_location(state_abbr)
+        parse_county_issuer_location(state_abbr)
 
 
 parse_by_state()
