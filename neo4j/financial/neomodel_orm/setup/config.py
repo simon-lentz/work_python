d160ac6b0314bed7bd4a4571327efed319ace4cb
simon-lentz <simonlentz1@gmail.com>
2024-05-01 14:54:09 -0400

    reorganize orm

--- neo4j/financial/neomodel_orm/setup/config.py
+++ neo4j/financial/neomodel_orm/setup/config.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Optional
 from pydantic import BaseModel
-from neomodel import config
+from neomodel import db, config
 
 logger = logging.getLogger(__name__)
 
@@ -16,9 +16,13 @@ class Neo4jConfig(BaseModel):
 
 def make_db_connection() -> bool:
     try:
-        db = Neo4jConfig()
-        config.DATABASE_URL = f"bolt://{db.username}:{db.password}@{db.address}:{db.bolt_port}"
+        server = Neo4jConfig()
+        config.DATABASE_URL = f"bolt://{server.username}:{server.password}@{server.address}:{server.bolt_port}"
+        if server.database_name:
+            config.DATABASE_NAME = server.database_name
+        results, _ = db.cypher_query("RETURN 'Connection Successful' as message")
+        logger.info(results)
         return True
     except Exception as e:
         logger.error(f"Failed to connect to db: {e}")
-        return False
+        return None
