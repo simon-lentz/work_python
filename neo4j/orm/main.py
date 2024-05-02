1f2d90373009b4715b2b0b3242f802d2654b492c
simon-lentz <simonlentz1@gmail.com>
2024-05-02 09:58:22 -0400

    Reorganize dirs, fix orm logging

--- neo4j/orm/main.py
+++ neo4j/orm/main.py
@@ -7,7 +7,14 @@ from query.apoc import (
     retrieve_city_nodes, retrieve_county_nodes, retrieve_state_nodes
 )
 
-logger = logging.Logger("neo4j financial")
+# Configure logging
+logger = logging.getLogger("Neo4j ORM")
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 
 def sample_cypher_queries():
@@ -23,7 +30,7 @@ def sample_apoc_queries():
 
 
 def main():
-    if make_db_connection():
+    if make_db_connection(logger):
         sample_cypher_queries()
         sample_apoc_queries()
     else:
--- neo4j/orm/setup/config.py
+++ neo4j/orm/setup/config.py
@@ -1,9 +1,6 @@
 import logging
-from typing import Optional
 from pydantic import BaseModel
 from neomodel import db, config
-
-logger = logging.getLogger(__name__)
 
 
 class Neo4jConfig(BaseModel):
@@ -11,17 +8,16 @@ class Neo4jConfig(BaseModel):
     password: str = "alaskadb"
     address: str = "localhost"
     bolt_port: str = "7687"
-    database_name: Optional[str] = None
+    database_name: str = "neo4j"
 
 
-def make_db_connection() -> bool:
+def make_db_connection(logger: logging.Logger) -> bool:
     try:
         server = Neo4jConfig()
         config.DATABASE_URL = f"bolt://{server.username}:{server.password}@{server.address}:{server.bolt_port}"
-        if server.database_name:
-            config.DATABASE_NAME = server.database_name
-        results, _ = db.cypher_query("RETURN 'Connection Successful' as message")
-        logger.info(results)
+        use_graph_query = f"USE {server.database_name}"
+        connected, _ = db.cypher_query(f"{use_graph_query}\nRETURN 'Connection Successful' as message")
+        logger.info(connected)
         return True
     except Exception as e:
         logger.error(f"Failed to connect to db: {e}")
